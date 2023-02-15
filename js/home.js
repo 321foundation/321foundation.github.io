4ac9e707aeac58abf229be0d213a5d0799c44c96
Shivam Soni <shivamsonidev@gmail.com>
2023-02-15 22:10:53 +0530

    takes the journey slider LIVE for Home and About page

--- js/home.js
+++ js/home.js
@@ -1,28 +1,4 @@
 $(document).ready(function () {
-    var swiper1 = new Swiper('#timeline .swiper-container', {
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-        spaceBetween: 30,
-        nextButton: '.timeline.swiper-button-next',
-        prevButton: '.timeline.swiper-button-prev',
-        grabCursor: true,
-        navigation: {
-            nextEl: '.timeline.swiper-button-next',
-            prevEl: '.timeline.swiper-button-prev',
-        },
-        pagination: '.timeline.swiper-pagination',
-        paginationClickable: true,
-        speed: 2000,
-        breakpoints: {
-            768: {
-                spaceBetween: 15,
-                slidesPerGroup: 1,
-                slidesPerView: 1,
-                freeMode: true,
-                loop: false,
-            }
-        },
-    });
     var swiper2 = new Swiper('#journey .swiper-container', {
         slidesPerView: 3,
         slidesPerGroup: 1,
